8bd0488a1e0dcf93e25e5990a820b5c141473136
yqh <395041293@qq.com>
2017-05-17 13:27:14 +0800

    引入flex 布局

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -1,7 +1,6 @@
 /*
  * Created by https://github.com/YMFL on 2017/5/16.
  */
-
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import './style.less'
@@ -11,22 +10,21 @@ class HomeHeader extends React.Component {
         super();
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     }
-
     render() {
         return (
             <div className="clear-fix HomeHeader">
-                <div className="home-header-left float-left">
+                <div className="home-header-left">
                     <span>深圳</span>&nbsp;
                     <i className="icon-angle-down"></i>
                 </div>
-                <div className="home-header-right float-right">
-                    <i className="icon-user"></i>
-                </div>
                 <div className="home-header-middle">
                     <div className="search-container">
-                        <i className="icon-search"></i>
                         <input placeholder="请输入关键字" type="text"/>
+                        <i className="icon-search"></i>
                     </div>
+                </div>
+                <div className="home-header-right ">
+                    <i className="icon-user"></i>
                 </div>
             </div>
         )
